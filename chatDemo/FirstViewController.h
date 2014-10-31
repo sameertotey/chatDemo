00c816591bdf4ba120a60746666667d69e91a26d
Sameer Totey <sameertotey@Sameers-MacBook-Pro.local>
2014-10-30 18:08:46 -0600

    Initial Commit

---
+++ chatDemo/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  chatDemo
+//
+//  Created by Sameer Totey on 10/30/14.
+//  Copyright (c) 2014 Sameer Totey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
---
+++ chatDemo/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  chatDemo
+//
+//  Created by Sameer Totey on 10/30/14.
+//  Copyright (c) 2014 Sameer Totey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
